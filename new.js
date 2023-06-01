ef3ac5a76db63813310788ce5c958f9b91266b5e
Prateek Chatterjee <41088357+ppchatterjee123@users.noreply.github.com>
2023-06-01 07:06:12 -0400

    w
    s

--- new.js
+++ new.js
@@ -1,21 +1,12 @@
 
-    $(document).ready(function() {
-        $(".animsition").animsition({
-          inClass               : 'fade-in',
-        outClass              :   'fade-out',
-        inDuration            :    1500,
-        outDuration           :    800,
-        linkElement           :   '.animsition-link',
-        // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-        loading               :    false,
-        loadingParentElement  :   false, //animsition wrapper element
-        loadingClass          :   false,
-        unSupportCss          : [     ],
-        //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-        //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-        overlay               :   false,
-        overlayClass          :   'animsition-overlay-slide',
-        overlayParentElement  :   'body'
-      });
-    });
-
+  $(document).ready(function() {
+  //Preloader
+  $(window).on("load", function() {
+  preloaderFadeOutTime = 10;
+  function hidePreloader() {
+  var preloader = $('.spinner-wrapper');
+  preloader.fadeOut(preloaderFadeOutTime);
+  }
+  hidePreloader();
+  });
+  });
